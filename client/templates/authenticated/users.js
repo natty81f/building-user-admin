0f6613b626fe7c430a386b060a868ba9890d1914
Natalia Fierro <nataliabfierro@gmail.com>
2016-01-31 17:11:24 -0800

    sets up event handler and method to change user roles

--- client/templates/authenticated/users.js
+++ client/templates/authenticated/users.js
@@ -22,3 +22,29 @@ Template.users.helpers({
     }
   }
 });
+
+Template.users.events({
+  'change [name="userRole"]': function( event, template ) {
+    let role = $( event.target ).find( 'option:selected' ).val();
+
+    Meteor.call( "setRoleOnUser", {
+      user: this._id,
+      role: role
+    }, ( error, response ) => {
+      if ( error ) {
+        Bert.alert( error.reason, "warning" );
+      }
+    });
+  },
+  'click .revoke-invite': function( event, template ) {
+    if ( confirm( "Are you sure? This is permanent." ) ) {
+      Meteor.call( "revokeInvitation", this._id, function( error, response ) {
+        if ( error ) {
+          Bert.alert( error.reason, "warning" );
+        } else {
+          Bert.alert( "Invitation revoked!", "success" );
+        }
+      });
+    }
+  }
+});
